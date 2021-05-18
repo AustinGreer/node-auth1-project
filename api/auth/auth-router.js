bb2fafdd6c18e53468e2c3935221266a8e4d3ee0
Austin Greer <agreer.ag12@gmail.com>
2021-05-17 23:08:24 -0400

    scaffolding for all endpoints complete

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,6 @@
 // Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
 // middleware functions from `auth-middleware.js`. You will need them here!
-
+const router = require('express').Router()
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
@@ -25,6 +25,10 @@
   }
  */
 
+router.post('/register', (req, res, next) => {
+  res.json({message: 'post new user'})
+})
+
 
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
@@ -42,6 +46,10 @@
   }
  */
 
+router.post('/login', (req, res, next) => {
+  res.json({message: 'user login'})
+})
+
 
 /**
   3 [GET] /api/auth/logout
@@ -58,6 +66,9 @@
     "message": "no session"
   }
  */
+router.get('/logout', (req, res, next) => {
+  res.json({message: 'user logout'})
+})
 
- 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const helmet = require("helmet");
 const cors = require("cors");
 
 const userRouter = require('./users/users-router')
+const authRouter = require('./auth/auth-router')
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -28,6 +29,7 @@ server.get("/", (req, res) => {
 });
 
 server.use('/api/users', userRouter)
+server.use('/api/auth', authRouter)
 
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
